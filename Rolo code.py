939d3eeed8371859de6870db1fce9475a276bf57
NikkiRalston <94995862+NikkiRalston@users.noreply.github.com>
2025-04-28 12:32:48 -0700

    Add files via upload

---
+++ Rolo code.py
@@ -0,0 +1,21 @@
+import os 
+
+def speak(file_name):
+    print("Playing once..."),
+    os. system (f'mpg321 "/home/Rolo_Project/Templates/Rolo/{file_name}"'),
+
+speak("Ask my friend.mp3")
+speak("Beep Beep.mp3")
+speak("Can you say that again.mp3")
+speak("Cool.mp3")
+speak("Follow me.mp3")
+speak("Hi, Im Rolo welcome to UAT.mp3")
+speak("I dont know.mp3")
+speak("Im green.mp3")
+speak("Innovate for the future.mp3")
+speak("Personal tour guide.mp3")
+speak("Sounds great.mp3")
+speak("STEM.mp3")
+speak("Uh oh.mp3")
+speak("Whats up.mp3")
+speak("Where you belong.mp3")
